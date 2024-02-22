bccd15ead73c761ebb1d0e7f4d1a9afa92989924
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-02-22 14:04:45 +0900

    랜덤 코드 생성기 추가

---
+++ src/main/java/com/multirkh/study_validation_mail/RandomStringGenerator.java
@@ -0,0 +1,23 @@
+package com.multirkh.study_validation_mail;
+
+import java.util.Random;
+
+public class RandomStringGenerator {
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    private static final int LENGTH = 64;
+
+    public static void main(String[] args) {
+        String randomString = generateRandomString(LENGTH);
+        System.out.println("Random String: " + randomString);
+    }
+
+    public static String generateRandomString(int length) {
+        Random random = new Random();
+        StringBuilder stringBuilder = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            int randomIndex = random.nextInt(CHARACTERS.length());
+            stringBuilder.append(CHARACTERS.charAt(randomIndex));
+        }
+        return stringBuilder.toString();
+    }
+}
