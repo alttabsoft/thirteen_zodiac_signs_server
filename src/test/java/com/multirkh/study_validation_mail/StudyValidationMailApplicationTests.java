83329b2f4caff06a3a1d6772048fb0f09e6925d0
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-02-22 09:04:43 +0900

    Init Project

---
+++ src/test/java/com/multirkh/study_validation_mail/StudyValidationMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multirkh.study_validation_mail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyValidationMailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
