246e705811bd4fb8231b1df934ea856dccf0d34a
Kihyun <69492686+leeve1247@users.noreply.github.com>
2024-02-23 12:12:54 +0900

    Add: 토큰 설명 정리

--- src/main/java/com/multirkh/study_validation_mail/filter/JWTTokenGeneratorFilter.java
+++ src/main/java/com/multirkh/study_validation_mail/filter/JWTTokenGeneratorFilter.java
@@ -28,21 +28,34 @@ import static com.multirkh.study_validation_mail.config.SecurityConstants.JWT_KE
 public class JWTTokenGeneratorFilter extends OncePerRequestFilter {
     Logger log = LoggerFactory.getLogger(this.getClass().getName());
 
+    /**
+     *
+     * @brief issuer :       해당 토큰을 발행하는 주체 <br/>
+     * subject : 소제목이라 아무거나 넣어도 됨 <br/>
+     * claim : 각 속성(attribute) 에 대한 값을 넣는 부분 <br/>
+     * issuedAt : 토큰 발행일 <br/>
+     * expiration : 토큰 만료일 //<br/>
+     * signwith(key) :  서명값 (사전 설정한 JWT_KEY 값을 활용하여 저장한다. <br/>
+     * &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;해당 키 값은 숨기는 것이 좋다.)
+     * compact : 최종 구성하여 String 으로 return;
+     *
+     */
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (null != authentication) {
             log.info("/////////  JwtKey = {} ////////////", JWT_KEY);
-            SecretKey key = Keys.hmacShaKeyFor(JWT_KEY.getBytes(StandardCharsets.UTF_8)); //키 발행
+            SecretKey key = Keys.hmacShaKeyFor(JWT_KEY.getBytes(StandardCharsets.UTF_8));
             String jwt = Jwts.builder()
-                    .issuer(COMPANY_NAME) // 해당 토큰을 발행하는 주체
-                    .subject("JWT Token") // 소제목이라 아무거나 넣어도 됨
-                    .claim("username", authentication.getName()) //인증과정에서 넘어온 유저이름, 비밀번호는 있으면 안됨, jwt 토큰을 쉽게 디코딩할 수 있음(이게 비밀번호가 존재하면 JWT 디코딩 난이도가 쉬워진다는 것인지, 아니면, JWT 디코딩 난이도가 쉬워서 비밀번호가 노출되기 쉽다는 의미인지는 잘 모르겠음), 그리고 애플리케이션은 이메일, 유저 이름, 권한등은 요구할 수 있으나, 비밀번호는 요구해서는 안됨.
-                    .claim("authorities", populateAuthorities(authentication.getAuthorities())) // 인증과정에서 넘어온 권한 모음
-                    .issuedAt(new Date()) // 토큰 발행일
-                    .expiration(new Date((new Date()).getTime() + 30000000)) // 토큰 만료일
-                    .signWith(key).compact(); // 서명값 (사전 설정한 JWT_KEY 값을 활용하여 저장한다. 해당 키 값은 숨기는것이 좋다.)
+                    .issuer(COMPANY_NAME)
+                    .subject("JWT Token")
+                    .claim("username", authentication.getName())
+                    .claim("authorities", populateAuthorities(authentication.getAuthorities()))
+                    .issuedAt(new Date())
+                    .expiration(new Date((new Date()).getTime() + 30000000))
+                    .signWith(key)
+                    .compact();
             response.setHeader(SecurityConstants.JWT_HEADER, jwt);
         }
 
